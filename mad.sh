5d7981ba3a446f5b743764db416da9eaeb51c6a7
vianel <v@ib.com>
2021-04-06 12:19:52 +0200

    first commit

---
+++ mad.sh
@@ -0,0 +1,13 @@
+echo "veuiller entre un nom" 
+
+read nom
+
+echo "veuiller entrer un verbe "
+
+read verbe 
+
+echo "veuillez entrer un adjectif "
+
+read adjectif 
+
+echo "je m'appelle $nom et  je  $verbe car je $adjectif
